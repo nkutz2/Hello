aaca60981a34cca4df4a5ea6f2b014ce468971dc
nkutz2 <54672482+nkutz2@users.noreply.github.com>
2019-08-29 08:54:06 -0500

    Initial commit

---
+++ Hello.c
@@ -0,0 +1,12 @@
+/* Nicholas kutz
+8/29/19
+This code generates a hello world text*/
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(in argc, char **argv)  {
+
+printf("Nick Kutz");
+
+return 0;
+}
